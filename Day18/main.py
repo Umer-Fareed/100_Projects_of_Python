fb9a1c0eace5177e9caa31490e69fa0e194002d4
Umer-Fareed <muhammad.umer.5121124@gmail.com>
2025-05-25 01:04:03 +0500

    it's day 18 and i am playing with oops concepts

--- Day18/main.py
+++ Day18/main.py
@@ -1,14 +1,53 @@
-import turtle
-from turtle import Turtle
+# import turtle
+#from turtle import Turtle
+#alias name
+from turtle import Turtle as t
 from turtle import Screen
+import random
 
-jimmy_the_turtle = Turtle()
-jimmy_the_turtle.shape("turtle")
-turtle.color("red")
+timm = t()
+# timm.shape("turtle")
+# t.color("red")
+# for _ in range(4):
+#     timm.forward(100)
+#     timm.left(90)
 
 
+# for _ in range(15):
+#     timm.forward(10)
+#     timm.penup()
+#     timm.forward(10)
+#     timm.pendown()
 
 
+# for _ in range(3):
+#     timm.forward(100)
+#     timm.right(120)
+#     timm.color("green")
+# for _ in range(4):
+#     timm.forward(100)
+#     timm.right(90)
+#     timm.color("red")
+# for _ in range(5):
+#     timm.forward(100)
+#     timm.right(72)
+#     timm.color("blue")
+# for _ in range(6):
+#     timm.forward(100)
+#     timm.right(60)
+#     timm.color("black")
 
-screen = Screen()
-screen.exitonclick()
+
+# colours = ["navy blue","black","blue","green","yellow","purple","black"]
+# def draw_shape(num_sides):
+#     angle = 360 / (num_sides)
+#     for _ in range(num_sides):
+#         timm.forward(100)
+#         timm.right(angle)
+#
+#
+# for num_of_sides in range(3,11):
+#     timm.color(random.choice(colours))
+#     draw_shape(num_of_sides)
+# screen = Screen()
+# screen.exitonclick()
