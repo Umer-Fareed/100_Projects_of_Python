9dfe2dd9d00c5f4eca7d92f2b5d139f0fb9ceb83
Umer-Fareed <muhammad.umer.5121124@gmail.com>
2025-05-25 00:15:06 +0500

    it's day 18 and i am playing with oops concepts

---
+++ Day18/main.py
@@ -0,0 +1,14 @@
+import turtle
+from turtle import Turtle
+from turtle import Screen
+
+jimmy_the_turtle = Turtle()
+jimmy_the_turtle.shape("turtle")
+turtle.color("red")
+
+
+
+
+
+screen = Screen()
+screen.exitonclick()
