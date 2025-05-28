f2226d854f30b9503e74268f3626decf4cbd6390
Umer-Fareed <muhammad.umer.5121124@gmail.com>
2025-05-28 23:33:10 +0500

    it's day 20 and i am making a snake game

---
+++ Day20/snake game.py
@@ -0,0 +1,24 @@
+from turtle import Turtle, Screen
+
+
+screen = Screen()
+screen.setup(600, 600)
+screen.title("My Snake game ")
+screen.bgcolor("black")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+screen.exitonclick()
