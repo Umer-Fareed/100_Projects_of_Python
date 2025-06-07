4d49657b08d89f7a51e09911e7714197266a9906
Umer-Fareed <muhammad.umer.5121124@gmail.com>
2025-06-07 10:39:13 +0500

    day 22 and i am making ping-pong game

--- Day22/ping_pong_game.py
+++ Day22/ping_pong_game.py
@@ -2,6 +2,8 @@ from turtle import Turtle, Screen
 from paddle import Paddle
 from ball import Ball
 import time
+from scoreboard import ScoreBoard
+
 
 screen = Screen()
 screen.bgcolor("black")
@@ -12,6 +14,8 @@ screen.tracer(0)
 r_paddle = Paddle((350, 0))
 l_paddle = Paddle((-350, 0))
 ball = Ball()
+scoreboard = ScoreBoard()
+
 
 
 
@@ -23,7 +27,7 @@ screen.onkey(l_paddle.go_down,"s")
 
 game_is_on = True
 while game_is_on:
-    time.sleep(0.1)
+    time.sleep(ball.move_speed)
     screen.update()
     ball.move()
 
@@ -40,9 +44,11 @@ while game_is_on:
     #detect R paddle misses
     if ball.xcor() > 380:
         ball.reset_position()
+        scoreboard.l_point()
 
     #detect L paddle misses
-    if ball.ycor() < -380:
+    if ball.xcor() < -380:
         ball.reset_position()
+        scoreboard.r_point()
 
 screen.exitonclick()
