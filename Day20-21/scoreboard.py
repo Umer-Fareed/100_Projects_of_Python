90757a98fe85219fe446ca58cdc495db05c596f3
Umer-Fareed <muhammad.umer.5121124@gmail.com>
2025-06-09 23:23:17 +0500

    day 24

--- Day20-21/scoreboard.py
+++ Day20-21/scoreboard.py
@@ -8,7 +8,8 @@ class Scoreboard(Turtle):
         super().__init__()
         self.penup()
         self.score = 0
-        self.high_score = 0
+        with open("data.txt") as data:
+            self.high_score=int(data.read())
         self.color("white")
         self.goto(0,260)
         self.update_scoreboard()
@@ -22,6 +23,8 @@ class Scoreboard(Turtle):
     def reset(self):
         if self.score > self.high_score:
             self.high_score = self.score
+            with open("data.txt", mode= "w") as data:
+                data.write(f"{self.high_score}")
         self.score = 0
         self.update_scoreboard()
 
