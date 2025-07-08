995852edb19d8451454e0dbe531bdbc5b59fdb22
Umer-Fareed <muhammad.umer.5121124@gmail.com>
2025-07-08 10:27:29 +0500

    day 32

---
+++ Day32/main.py
@@ -0,0 +1,30 @@
+# # import smtplib
+# #
+# # my_email = "experimental.my2@gmail.com"
+# # password= "obgpgxtiflrlcsfa"
+# # connection= smtplib.SMTP("smtp.gmail.com")
+# # connection.starttls()
+# # connection.login(user=my_email , password= password)
+# # connection.sendmail(from_addr=my_email,
+# #                     to_addrs="experimental.my2@yahoo.com",
+# #                     msg="subject:Hello\n\n hi my name is umer and i am writting an email")
+# # connection.close()
+#
+#
+# import datetime as dt
+#
+# now= dt.datetime.now()
+# year= now.year
+# print(year)
+# day_of_week= now.weekday()
+# print(day_of_week)
+# print(now)
+#
+#
+# date_of_birth= dt.datetime(year= 2002, month=8 , day=16 )
+# print(date_of_birth)
+
+
+#Birthdat Wisher Project
+
+
