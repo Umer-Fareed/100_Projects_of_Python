8e49db62d1407bd146df1c62e5200a8e16d58ecb
Umer-Fareed <muhammad.umer.5121124@gmail.com>
2025-07-21 00:04:59 +0500

    day 37

--- Day37/Habit_tracker.py
+++ Day37/Habit_tracker.py
@@ -1,8 +1,12 @@
 import  requests
+from datetime import datetime
+
 
 USER_NAME= "umer-fareed"
 TOKEN= "asdfrq5jn234n234112"
 pixela_endpoint= "https://pixe.la/v1/users"
+GRAPH_ID=  "graph786"
+
 
 user_params= {
     "token": TOKEN,
@@ -16,7 +20,7 @@ user_params= {
 graph_endpoint= f"{pixela_endpoint}/{USER_NAME}/graphs"
 
 graph_config= {
-    "id": "graph786",
+    "id":GRAPH_ID,
     "name" : "Study Graph",
     "unit" : "minute",
     "type" : "int",
@@ -30,3 +34,23 @@ headers= {
 # print(response.text)
 
 
+pixel_creation_endpoint= f"{pixela_endpoint}/{USER_NAME}/graphs/{GRAPH_ID}"
+
+today= datetime.now()
+pixel_data= {
+    "date": today.strftime("%Y%m%d"),
+    "quantity":input("how much time you spend on learning today: \n")
+}
+
+response= requests.post(url= pixel_creation_endpoint, json=pixel_data, headers=headers)
+print(response.text)
+
+update_endpoint= f"{pixela_endpoint}/{USER_NAME}/graphs/{GRAPH_ID}/{today.strftime("%Y%m%d")}"
+
+new_pixel_data= {
+    "quantity": "240"
+
+}
+
+# response= requests.put(url=update_endpoint,json=new_pixel_data, headers=headers)
+# print(response.text)
