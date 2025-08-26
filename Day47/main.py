45399fd1ed7271ed87f84206aaa3c48dab572ffb
Umer-Fareed <muhammad.umer.5121124@gmail.com>
2025-08-26 15:11:45 +0500

    day 47 start

--- Day47/main.py
+++ Day47/main.py
@@ -1,55 +1,44 @@
 from bs4 import BeautifulSoup
 import requests
-from dotenv import load_dotenv
-import os
-load_dotenv()
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 
-client_id= os.getenv("CLIENT_ID")
-client_secret= os.getenv("CLIENT_SECRET")
-
+# Scraping Billboard 100
 date = input("Which year do you want to travel to? Type the date in this format YYYY-MM-DD: ")
-
 response = requests.get("https://www.billboard.com/charts/hot-100/" + date)
-
 soup = BeautifulSoup(response.text, 'html.parser')
-
 song_names_spans = soup.select("li ul li h3")
 song_names = [song.getText().strip() for song in song_names_spans]
-print(song_names)
+
+#Spotify Authentication
 sp = spotipy.Spotify(
     auth_manager=SpotifyOAuth(
         scope="playlist-modify-private",
-        redirect_uri="https://fareed787.com",
-        client_id=client_id,
-        client_secret=client_secret,
+        redirect_uri="http://example.com",
+        client_id=YOUR CLIENT ID,
+        client_secret=YOUR CLIENT SECRET,
         show_dialog=True,
         cache_path="token.txt"
     )
 )
 user_id = sp.current_user()["id"]
+print(user_id)
 
-#ID= "31x6oodbe6ixbvasxeii4vgd6mja"
-
-
-song_uris = []
+#Searching Spotify for songs by title
+song_urls = []
 year = date.split("-")[0]
 for song in song_names:
     result = sp.search(q=f"track:{song} year:{year}", type="track")
     print(result)
     try:
         uri = result["tracks"]["items"][0]["uri"]
-        song_uris.append(uri)
+        song_urls.append(uri)
     except IndexError:
         print(f"{song} doesn't exist in Spotify. Skipped.")
 
-
+#Creating a new private playlist in Spotify
 playlist = sp.user_playlist_create(user=user_id, name=f"{date} Billboard 100", public=False)
 print(playlist)
 
-sp.playlist_add_items(playlist_id=playlist["id"], items=song_uris)
-
-
-
-
+#Adding songs found into the new playlist
+sp.playlist_add_items(playlist_id=playlist["id"], items=song_urls)
